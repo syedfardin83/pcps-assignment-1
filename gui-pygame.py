bc3f51aaaf27d645afb29a3c071bc845ddfd0a11
Fardin <syedfardin0612@gmail.com>
2025-10-22 17:27:57 +0530

    Hunter and heal button added

--- gui-pygame.py
+++ gui-pygame.py
@@ -15,12 +15,20 @@ attack_button_image = pygame.image.load('./images/attack_button.png')
 attack_button_image = pygame.transform.scale(attack_button_image,(100,100))
 attack_button_rect = attack_button_image.get_rect(topleft=(window_size[0]-150,window_size[1]-150))
 
+heal_button_image = pygame.image.load('./images/heal_button.png')
+heal_button_image = pygame.transform.scale(heal_button_image,(100,100))
+heal_button_rect = attack_button_image.get_rect(topleft=(window_size[0]-270,window_size[1]-150))
+
 background_image = pygame.image.load('./images/forest_retro.png')
 background_image = pygame.transform.scale(background_image,window_size)
 
 self_image = pygame.image.load('./images/robin_hood.webp')
 self_image = pygame.transform.scale(self_image,(100,200))
 self_image_rect = self_image.get_rect(topleft=(window_size[0]-150,window_size[1]-400))
+
+hunter_image = pygame.image.load('./images/hunter.png')
+hunter_image = pygame.transform.scale(hunter_image,(200,200))
+hunter_image_rect = self_image.get_rect(topleft=(100,window_size[1]-400))
 
 
 
@@ -36,11 +44,15 @@ while not exit_game:
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             if attack_button_rect.collidepoint(event.pos):
-                print("Button clicked!!")
+                print("Attack Button clicked!!")
+            if heal_button_rect.collidepoint(event.pos):
+                print("Heal button clicked!")
 
     gameWindow.blit(background_image,(0,0))
     gameWindow.blit(attack_button_image,attack_button_rect)
+    gameWindow.blit(heal_button_image,heal_button_rect)
     gameWindow.blit(self_image,self_image_rect)
+    gameWindow.blit(hunter_image,hunter_image_rect)
 
     pygame.display.flip()
 
