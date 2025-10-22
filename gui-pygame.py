a236f6e0b017b004f341913510d18ebe9516ce89
Fardin <syedfardin0612@gmail.com>
2025-10-22 16:58:43 +0530

    Self and attack button position fixed;

--- gui-pygame.py
+++ gui-pygame.py
@@ -13,12 +13,14 @@ exit_game = False
 
 attack_button_image = pygame.image.load('./images/attack_button.png')
 attack_button_image = pygame.transform.scale(attack_button_image,(100,100))
-attack_button_rect = attack_button_image.get_rect(topleft=(150,150))
+attack_button_rect = attack_button_image.get_rect(topleft=(window_size[0]-150,window_size[1]-150))
 
 background_image = pygame.image.load('./images/forest_retro.png')
 background_image = pygame.transform.scale(background_image,window_size)
 
 self_image = pygame.image.load('./images/robin_hood.webp')
+self_image = pygame.transform.scale(self_image,(100,200))
+self_image_rect = self_image.get_rect(topleft=(window_size[0]-150,window_size[1]-400))
 
 
 
@@ -38,6 +40,7 @@ while not exit_game:
 
     gameWindow.blit(background_image,(0,0))
     gameWindow.blit(attack_button_image,attack_button_rect)
+    gameWindow.blit(self_image,self_image_rect)
 
     pygame.display.flip()
 
